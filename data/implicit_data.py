df708186c3afba964f74b62dc25c00fd9d2e8cb7
João Vinagre <joaoms@joaoms.com>
2021-02-09 15:47:53 +0000

    Change numpy > native lists

--- data/implicit_data.py
+++ data/implicit_data.py
@@ -3,8 +3,8 @@ import numpy as np
 class ImplicitData:
 
     def __init__(self, user_list: list, item_list: list):
-        self.userlist = np.array(user_list)
-        self.itemlist = np.array(item_list)
+        self.userlist = user_list
+        self.itemlist = item_list
         self.size = len(self.userlist)
         self.userset, self.userindices = np.unique(self.userlist, return_inverse=True)
         self.itemset, self.itemindices = np.unique(self.itemlist, return_inverse=True)
@@ -25,7 +25,7 @@ class ImplicitData:
 
     def GetUserItems(self, user_id, internal = True):
         if internal:
-            if user_id in self.userlist:
+            if user_id > -1 and user_id <= self.maxuserid:
                 return self.useritems[user_id]
             return []
         uid = self.GetUserInternalId(user_id)
@@ -35,7 +35,7 @@ class ImplicitData:
 
     def GetItemUsers(self, item_id, internal = True):
         if internal:
-            if item_id in self.itemlist:
+            if item_id > -1 and item_id <= self.maxitemid:
                 return self.itemusers[item_id]
         iid = self.GetItemInternalId(item_id)
         if iid > -1:
@@ -44,19 +44,19 @@ class ImplicitData:
 
     def AddFeedback(self, user, item):
         self.size = self.size + 1
-        self.userlist = np.append(self.userlist, user)
-        self.itemlist = np.append(self.itemlist, item)
+        self.userlist.append(user)
+        self.itemlist.append(item)
         if user not in self.userset:
             self.userset = np.append(self.userset, user)
             self.maxuserid = self.maxuserid + 1
             self.useritems.append([])
-        user_id, = np.where(self.userset == user)[0]
+        user_id = self.GetUserInternalId(user)
         self.userindices = np.append(self.userindices, user_id)
         if item not in self.itemset:
             self.itemset = np.append(self.itemset, item)
             self.maxitemid = self.maxitemid + 1
             self.itemusers.append([])
-        item_id, = np.where(self.itemset == item)[0]
+        item_id = self.GetItemInternalId(item)
         self.itemindices = np.append(self.itemindices, item_id)
         self.useritems[user_id].append(item_id)
         self.itemusers[item_id].append(user_id)
